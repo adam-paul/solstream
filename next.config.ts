61311edf19cd0cbd49a3b24ccad4193b99ee4bce
adam-paul <notadampaul@protonmail.com>
2024-12-12 19:36:08 -0700

    custom domain updates

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from 'next'
 
-const isProduction = process.env.NODE_ENV === 'production'
-
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProduction ? '/solstream' : '',
+  basePath: '',
   images: {
     unoptimized: true,
   },
