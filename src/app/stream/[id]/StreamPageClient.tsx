1fea680e3258d9358e4560eeba825e5d04758ea5
adam-paul <notadampaul@protonmail.com>
2025-01-03 21:29:12 -0700

    proper go back centering

--- src/app/stream/[id]/StreamPageClient.tsx
+++ src/app/stream/[id]/StreamPageClient.tsx
@@ -42,7 +42,7 @@ export default function StreamPageClient({ streamId }: StreamPageClientProps) {
       <div className="max-w-7xl mx-auto p-4">
         <button 
           onClick={() => router.push('/')}
-          className="text-white hover:font-bold text-2xl mb-12 transition-all text-center"
+          className="text-white hover:font-bold text-2xl mb-12 transition-all mx-auto"
         >
           [go back]
         </button>
